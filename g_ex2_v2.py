9a8224bb3c77ea2199c516ee90d32de5fc0750b8
neliubin.egor <neo.8318731@gmail.com>
2023-04-03 12:22:27 +0300

    ex2 p5

--- g_ex2_v2.py
+++ g_ex2_v2.py
@@ -1,8 +1,9 @@
 from operator import itemgetter
 import datetime
-import pandas as pd
+import csv
 
 RUN = True
+FILE = 's_list.txt'
 HELP = '''
 Available commands:
 /show <date>    - show all tasks for date (first 10 if date is empty)
@@ -18,22 +19,26 @@ Available commands:
 #2,2023-03-31,08:30,Test task n2,N,,ToDo
 
 list = []
-f_list = open('s_list.txt', 'r')
+f_list = open(FILE, 'r')
 for str in f_list :
     list.append(tuple(str.replace('\n','').split(',')))
 f_list.close()
 list.sort(key=itemgetter(0))
 max_task_in_file = int(list[-1][0])
+changed = False
 
 
-def check_date(t_date_in) :
+def check_date(msg) :
     date_format = '%Y-%m-%d'
-    try:
-        dateObject = datetime.datetime.strptime(t_date_in, date_format)
-        return True
-    except ValueError:
-        print('It\'s not correct date!')
-        return False
+    date_loop = True
+    while date_loop :
+        t_date_in = input(msg)
+        try:
+            dateObject = datetime.datetime.strptime(t_date_in, date_format)
+            date_loop = False
+        except ValueError:
+            print('It\'s not correct date!')
+    return t_date_in
 
 
 def check_time(msg) :
@@ -53,19 +58,20 @@ def check_time(msg) :
 
 
 def add_task() :
-    global max_task_in_file
+    global max_task_in_file, list, changed
     max_task_in_file +=1
     t_id = max_task_in_file
-    date_loop = True
-    while date_loop :
-        t_date = input('Input date (YYYY-MM-DD): ')
-        if check_date(t_date) :
-            date_loop = False
+    t_date = check_date('Input date (YYYY-MM-DD): ')
     t_time = check_time('Input task time (HH:MM): ')
     t_text = input('Input Task: ')
     t_not = input('Need notification? (input Y or N): ')
-    #if t_not == 'Y' :
-    #    not_time_loop
+    if t_not == 'Y' :
+        t_time_not = check_time('Input notification time (HH:MM): ')
+    else :
+        t_time_not = ''
+    t_status = 'ToDo'
+    list.append(tuple((t_id,t_date,t_time,t_text,t_not,t_time_not,t_status)))
+    changed = True
     
         
 def show() :
@@ -74,7 +80,11 @@ def show() :
 
 
 def save_list() :
-    print('xxx')
+    global list, changed
+    if changed :
+        with open(FILE, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(list)
 
 
 while RUN :
@@ -86,7 +96,6 @@ while RUN :
     else :
         cur_command=in_str[0:in_str.find(' ')]
         p_date = in_str[in_str.find(' ')+1:len(in_str)-1]
-
     if cur_command == '/exit' :
         save_list()
         RUN = False
